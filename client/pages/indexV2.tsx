01afa34a821b29c4ac6b38867c928f3a69cfbc05
Gary Xie <garyxie13@live.com>
2021-08-11 20:46:23 -0400

    Temporarily disable Editor import due to error with highlight.js

--- client/pages/indexV2.tsx
+++ client/pages/indexV2.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import dynamic from 'next/dynamic'
 import { useEffect } from 'react'
 import axios from 'axios'
-const Editor = dynamic(() => import('@/components/EditorV2'), { ssr: false })
+// const Editor = dynamic(() => import('@/components/EditorV2'), { ssr: false })
 
 const IndexV2Page: React.FC = () => {
   useEffect(() => {
@@ -11,7 +11,8 @@ const IndexV2Page: React.FC = () => {
       .then((response) => console.log(response))
       .catch((error) => console.error(error))
   }, [])
-  return <Editor />
+  return null
+  // return <Editor />
 }
 
 export default IndexV2Page
