4e0cbdb8986793a7f25ce0bb56c020a2be4beb81
Gary Xie <garyxie13@live.com>
2021-08-11 21:31:27 -0400

    Update config

--- server/pkg/config/config.go
+++ server/pkg/config/config.go
@@ -1,7 +1,6 @@
 package config
 
 import (
-	"log"
 	"os"
 
 	"github.com/joho/godotenv"
@@ -14,10 +13,7 @@ type EnvVar struct {
 var Env EnvVar
 
 func SetupConfig() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	godotenv.Load()
 
 	Env.ClientUrl = os.Getenv("CLIENT_URL")
 }
