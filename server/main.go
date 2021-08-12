6131068ad09e78144d52b56ffe84137bcd5dbe0a
Gary Xie <garyxie13@live.com>
2021-08-12 12:37:58 -0400

    Update route logic

--- server/main.go
+++ server/main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"log"
 	"os"
-	"strings"
 
 	"github.com/GaryJX/code-cube/pkg/config"
+	"github.com/GaryJX/code-cube/pkg/routes"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 )
@@ -16,41 +16,11 @@ func main() {
 
 	app := fiber.New()
 
-	// TODO: Change cors config later, currently it allows all origins
 	app.Use(cors.New(cors.Config{
 		AllowOrigins: config.Env.ClientUrl,
 	}))
 
-	app.Get("/api", func(c *fiber.Ctx) error {
-		return c.JSON(fiber.Map{
-			"message": "Successfully reached API endpoint",
-		})
-	})
-
-	app.Get("/health", func(c *fiber.Ctx) error {
-		return c.JSON(fiber.Map{
-			"message": "Server is up and running!",
-		})
-	})
-
-	app.Get("/test", func(c *fiber.Ctx) error {
-
-		authToken := string(c.Request().Header.Peek("Authorization"))
-		splitToken := strings.Split(authToken, "Bearer ")
-
-		// TODO: Move the logic for checking for Bearer auth token to a middleware
-		if len(splitToken) != 2 {
-			return c.Status(400).JSON(fiber.Map{
-				"message": "Incorrect authorization format",
-			})
-		}
-
-		return c.JSON(fiber.Map{
-			"message":    "Test Endpoint",
-			"authToken":  authToken,
-			"splitToken": splitToken[1],
-		})
-	})
+	routes.SetupRoutes(app)
 
 	port := os.Getenv("PORT")
 	// Default port in development
--- server/pkg/routes/routes.go
+++ server/pkg/routes/routes.go
@@ -1,5 +1,40 @@
 package routes
 
-func routes() {
+import (
+	"strings"
 
+	"github.com/gofiber/fiber/v2"
+)
+
+func SetupRoutes(app *fiber.App) {
+	app.Get("/api", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"message": "Successfully reached API endpoint",
+		})
+	})
+
+	app.Get("/health", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"message": "Server is up and running!",
+		})
+	})
+
+	app.Get("/test", func(c *fiber.Ctx) error {
+
+		authToken := string(c.Request().Header.Peek("Authorization"))
+		splitToken := strings.Split(authToken, "Bearer ")
+
+		// TODO: Move the logic for checking for Bearer auth token to a middleware
+		if len(splitToken) != 2 {
+			return c.Status(400).JSON(fiber.Map{
+				"message": "Incorrect authorization format",
+			})
+		}
+
+		return c.JSON(fiber.Map{
+			"message":    "Test Endpoint",
+			"authToken":  authToken,
+			"splitToken": splitToken[1],
+		})
+	})
 }
