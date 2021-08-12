e6dfff0df520d2cdf0cf1751356c1cbf1738f962
Gary Xie <garyxie13@live.com>
2021-08-11 20:40:41 -0400

    Test deployment changes

--- client/pages/indexV2.tsx
+++ client/pages/indexV2.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import dynamic from 'next/dynamic'
+import { useEffect } from 'react'
+import axios from 'axios'
 const Editor = dynamic(() => import('@/components/EditorV2'), { ssr: false })
 
 const indexV2Page = () => {
+  useEffect(() => {
+    axios
+      .get('https://code-cube-api.herokuapp.com/api')
+      .then((response) => console.log(response))
+      .catch((error) => console.error(error))
+  }, [])
   return <Editor />
 }
 
