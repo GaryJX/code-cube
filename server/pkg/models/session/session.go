8fef07065b73b8ed142840c871d552e041758cf2
Gary Xie <garyxie13@live.com>
2021-08-14 12:25:15 -0400

    Add Session Model

---
+++ server/pkg/models/session/session.go
@@ -0,0 +1,32 @@
+package session
+
+import (
+	"context"
+	"time"
+
+	"github.com/GaryJX/code-cube/pkg/database"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+const collection = "sessions"
+
+type Session struct {
+	ID           primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	UserID       primitive.ObjectID `json:"userId,omitempty" bson:"userId,omitempty"`
+	Expires      primitive.DateTime `json:"expires,omitempty" bson:"expires,omitempty"`
+	SessionToken string             `json:"sessionToken,omitempty" bson:"sessionToken,omitempty"`
+	AccessToken  string             `json:"accessToken,omitempty" bson:"accessToken,omitempty"`
+	CreatedAt    primitive.ObjectID `json:"createdAt,omitempty" bson:"createdAt,omitempty"`
+	UpdatedAt    primitive.ObjectID `json:"updatedAt,omitempty" bson:"updatedAt,omitempty"`
+}
+
+func GetSession(accessToken string) (Session, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	var session Session
+	err := database.DB.Collection(collection).FindOne(ctx, bson.M{"accessToken": accessToken}).Decode(&session)
+
+	return session, err
+}
