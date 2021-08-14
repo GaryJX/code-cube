8c82c8b1d64c38175972f4b9a44070773b90df82
Gary Xie <garyxie13@live.com>
2021-08-13 22:31:44 -0400

    Add Cube model

---
+++ server/pkg/models/Cube.go
@@ -0,0 +1,28 @@
+package models
+
+import (
+	"context"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type Cube struct {
+}
+
+func (c *Cube) getCubes(db *mongo.Database, userId string) ([]Cube, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// TODO: Use the userId when filtering for cubes
+	cursor, err := db.Collection("cubes").Find(ctx, bson.M{})
+	if err != nil {
+		return []Cube{}, err
+	}
+
+	var cubes []Cube
+
+	err = cursor.All(ctx, &cubes)
+	return cubes, err
+}
