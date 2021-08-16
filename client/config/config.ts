a62f4d7b796b28c8efc40057bb0d5ac0462c2811
Gary Xie <garyxie13@live.com>
2021-08-16 19:39:57 -0400

    Add Google Oauth config

--- client/config/config.ts
+++ client/config/config.ts
@@ -2,6 +2,8 @@ const Config = {
   backendApiURL: process.env.NEXT_PUBLIC_BACKEND_API_URL!,
   githubClientID: process.env.GITHUB_CLIENT_ID!,
   githubClientSecret: process.env.GITHUB_CLIENT_SECRET!,
+  googleClientID: process.env.GOOGLE_CLIENT_ID!,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET!,
   mongodbDatabase: process.env.MONGODB_DB!,
   mongodbURI: process.env.MONGODB_URI!,
   nextAuthURL: process.env.NEXTAUTH_URL!,
