e9bbb1c4d6897151d177c18240d30dd4f8d75529
Gary Xie <garyxie13@live.com>
2021-08-15 22:33:08 -0400

    Add helper functions for auth middleware

--- server/pkg/router/handler/handler.go
+++ server/pkg/router/handler/handler.go
@@ -2,8 +2,8 @@ package handler
 
 import "github.com/gofiber/fiber/v2"
 
-// Helper function for sending back error responses
-func SendError(c *fiber.Ctx, status int, err string) error {
+// Helper function for sending back error responses (// TODO: Move to its own utils/helpers package)
+func sendError(c *fiber.Ctx, status int, err string) error {
 	return c.Status(status).JSON(fiber.Map{
 		"error": err,
 	})
--- server/pkg/router/middleware/middleware.go
+++ server/pkg/router/middleware/middleware.go
@@ -4,24 +4,38 @@ import (
 	"strings"
 
 	"github.com/GaryJX/code-cube/pkg/models/session"
-	"github.com/GaryJX/code-cube/pkg/router/handler"
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
+
+// Key for c.Locals to store User ID
+const userIDKey = "userID"
 
 func Auth(c *fiber.Ctx) error {
 	authToken := string(c.Request().Header.Peek("Authorization"))
 	splitToken := strings.Split(authToken, "Bearer ")
 	if len(splitToken) != 2 {
-		return handler.SendError(c, 401, "Missing Bearer Authorization token")
+		return sendError(c, 401, "Missing Bearer Authorization token")
 	}
 	// Extract access token from Bearer Authorization token
 	accessToken := splitToken[1]
 
-	s, err := session.GetSession(accessToken)
+	userSession, err := session.GetSession(accessToken)
 	if err != nil {
-		return handler.SendError(c, 401, "Session not found, please log in")
+		return sendError(c, 401, "Session not found, please log in")
 	}
 
-	c.Locals("userID", s.UserID)
+	c.Locals(userIDKey, userSession.UserID)
 	return c.Next()
+}
+
+func GetUserID(c *fiber.Ctx) primitive.ObjectID {
+	return c.Locals(userIDKey).(primitive.ObjectID)
+}
+
+// Helper function for sending back error responses (// TODO: Move to its own utils/helpers package)
+func sendError(c *fiber.Ctx, status int, err string) error {
+	return c.Status(status).JSON(fiber.Map{
+		"error": err,
+	})
 }
