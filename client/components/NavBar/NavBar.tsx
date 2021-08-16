1ff056027a0ea8b49352cfdf9a6c305aaec57270
Gary Xie <garyxie13@live.com>
2021-08-15 23:39:40 -0400

    Add log out button

--- client/components/NavBar/NavBar.tsx
+++ client/components/NavBar/NavBar.tsx
@@ -1,7 +1,13 @@
+import { signOut } from 'next-auth/client'
 import React from 'react'
 
 const NavBar = () => {
-  return <div>NavBar</div>
+  return (
+    <div>
+      NavBar
+      <button onClick={() => signOut()}>Log Out</button>
+    </div>
+  )
 }
 
 export default NavBar
