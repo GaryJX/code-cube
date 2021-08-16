edd8c02fdc35b160b1dbebb39c104689f9c93049
Gary Xie <garyxie13@live.com>
2021-08-15 22:31:48 -0400

    Update UI for displaying cube index page

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -7,16 +7,25 @@ import Loading from '@/components/Loading/Loading'
 import useAuth from '@/hooks/useAuth'
 import useServerAxios from '@/hooks/useServerAxios'
 import Layout from '@/components/Layout/Layout'
-const Editor = dynamic(() => import('@/components/CodeEditor/CodeEditor'), {
-  ssr: false,
-})
+
+type Cube = {
+  _id: string
+  creatorID: string
+  created: string
+  updated: string
+  name: string
+  html?: string
+  css?: string
+  js?: string
+  packages?: string[]
+}
 
 const IndexPage: React.FC = () => {
   const [session, sessionLoading] = useAuth()
   // TODO: Look into implementing the useApiResource hook I made for TI
   const [serverAxios, axiosLoading] = useServerAxios()
   const [loading, setLoading] = useState(true)
-  const [data, setData] = useState<any[]>([])
+  const [cubes, setCubes] = useState<Cube[]>([])
 
   useEffect(() => {
     if (!axiosLoading) {
@@ -24,7 +33,7 @@ const IndexPage: React.FC = () => {
         .get('/api/cubes')
         .then((response) => {
           console.log({ data: response.data })
-          setData(response.data)
+          setCubes(response.data)
           setLoading(false)
         })
         .catch((error) => {
@@ -39,7 +48,20 @@ const IndexPage: React.FC = () => {
 
   return (
     <Layout>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+      <div>
+        <div>
+          <button>Create New Cube</button>
+        </div>
+        {cubes.map((cube) => (
+          <div>
+            <a href={`/editor/${cube._id}`}>
+              <h2>Title: {cube.name}</h2>
+            </a>
+            <p>Created: {cube.created}</p>
+            <p>Updated: {cube.updated}</p>
+          </div>
+        ))}
+      </div>
     </Layout>
   )
 }
