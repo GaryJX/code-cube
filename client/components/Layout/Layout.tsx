ba16b882ab8ac65d314c996fc61bcc9b3b9c223d
Gary Xie <garyxie13@live.com>
2021-08-16 21:56:41 -0400

    Update Index Page layout

--- client/components/Layout/Layout.tsx
+++ client/components/Layout/Layout.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import NavBar from '@/components/NavBar/NavBar'
+import { Heading } from '@chakra-ui/react'
 
 const Layout: React.FC = ({ children }) => {
   return (
     <>
-      <NavBar />
+      <NavBar>
+        <Heading size="md">Code Cube</Heading>
+      </NavBar>
       {children}
     </>
   )
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -7,6 +7,18 @@ import Loading from '@/components/Loading/Loading'
 import useAuth from '@/hooks/useAuth'
 import useServerAxios from '@/hooks/useServerAxios'
 import Layout from '@/components/Layout/Layout'
+import {
+  Box,
+  Button,
+  Link,
+  Table,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from '@chakra-ui/react'
+import NextLink from 'next/link'
 
 export type Cube = {
   _id: string
@@ -49,6 +61,9 @@ const IndexPage: React.FC = () => {
         .delete(`/api/cube/${cubeID}`)
         .then((response) => {
           console.log({ data: response.data })
+          setCubes((prevCubes) =>
+            prevCubes.filter((cube) => cube._id !== cubeID)
+          )
         })
         .catch((error) => {
           console.error({ error })
@@ -74,21 +89,66 @@ const IndexPage: React.FC = () => {
 
   return (
     <Layout>
-      <div>
-        <div>
-          <button onClick={createNewCube}>Create New Cube</button>
-        </div>
-        {cubes.map((cube) => (
-          <div key={cube._id}>
-            <a href={`/editor/${cube._id}`}>
-              <h2>Title: {cube.name}</h2>
-            </a>
-            <p>Created: {cube.created}</p>
-            <p>Updated: {cube.updated}</p>
-            <button onClick={() => handleDelete(cube._id)}>Delete Cube</button>
-          </div>
-        ))}
-      </div>
+      <Box padding="2rem 5rem">
+        <Button onClick={createNewCube} colorScheme="blue" marginBottom="1rem">
+          Create New Cube
+        </Button>
+        <Table>
+          <Thead>
+            <Tr>
+              <Th color="white">Title</Th>
+              <Th color="white" width="1px">
+                Created
+              </Th>
+              <Th color="white" width="1px">
+                Last Updated
+              </Th>
+              <Th width="1px"></Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {cubes.length > 0 ? (
+              cubes.map((cube) => (
+                <Tr>
+                  <Td>
+                    <NextLink href={`/editor/${cube._id}`}>
+                      <Link color="blue.300">{cube.name}</Link>
+                    </NextLink>
+                  </Td>
+                  <Td whiteSpace="nowrap">
+                    {new Intl.DateTimeFormat('en-US', {
+                      dateStyle: 'long',
+                    }).format(new Date(cube.created))}
+                  </Td>
+                  <Td whiteSpace="nowrap">
+                    {new Intl.DateTimeFormat('en-US', {
+                      dateStyle: 'long',
+                    }).format(new Date(cube.updated))}
+                  </Td>
+                  <Td>
+                    <Button
+                      colorScheme="red"
+                      onClick={() => handleDelete(cube._id)}
+                    >
+                      Delete Cube
+                    </Button>
+                  </Td>
+                </Tr>
+              ))
+            ) : (
+              <Tr>
+                <Td colSpan={4}>
+                  No cubes found.{' '}
+                  <button onClick={createNewCube}>
+                    <Link color="blue.300">Create a new cube</Link>
+                  </button>
+                  .
+                </Td>
+              </Tr>
+            )}
+          </Tbody>
+        </Table>
+      </Box>
     </Layout>
   )
 }
